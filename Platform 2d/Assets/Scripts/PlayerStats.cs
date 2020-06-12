8442b7b51d8dcb8c5837cb587a9dca0ffbf416dc
AdusV <adriankomendera@interia.pl>
2020-06-12 21:04:54 +0200

    Dodanie pulapki i restartu gry

---
+++ Platform 2d/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+   [SerializeField] private int maxHp = 100;
+   public int hp;
+    private void Start()
+    {
+        hp = maxHp;
+    }
+    public void Damage(int damage)
+    {
+        hp -= damage;
+    }
+}
---
+++ Platform 2d/Assets/Scripts/Trap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    PlayerStats playerStats;
+    public int damage;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.GetComponent<PlayerStats>().Damage(damage);
+            Debug.Log("enemy");
+        }
+    }
+
+}
